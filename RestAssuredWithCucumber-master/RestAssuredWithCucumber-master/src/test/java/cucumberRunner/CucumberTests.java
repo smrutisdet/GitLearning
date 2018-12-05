dc42018f66072841bf3d619befc2acaf63be681b
smruti <smmohapatra011@gmail.com>
2018-12-05 18:14:03 +0530

    First commit in master branch

---
+++ RestAssuredWithCucumber-master/RestAssuredWithCucumber-master/src/test/java/cucumberRunner/CucumberTests.java
@@ -0,0 +1,13 @@
+package cucumberRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = {"pretty"},
+		glue = {"stepdefs"},
+		features = {"src/test/features"})
+public class CucumberTests {}
